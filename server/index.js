6bab5d3f7bf80c7bcdbdb17d3ac124f74424172c
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-14 11:34:41 +0300

    implement routing with empty controller

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
+const router = require("./router/index");
+
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +13,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+app.use("/api", router);
 
 const start = async () => {
   try {
