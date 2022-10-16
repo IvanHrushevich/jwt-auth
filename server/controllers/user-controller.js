a6a7a53297fd54950d9f274b48beef94e7ca3b35
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2022-10-16 18:49:48 +0300

    implement user activation by link

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -37,9 +37,11 @@ class UserController {
 
   async activate(req, res, next) {
     try {
-      //
+      const activationLink = req.params.link;
+      await userService.activate(activationLink);
+      return res.redirect(process.env.CLIENT_URL);
     } catch (error) {
-      //
+      console.log("error", error);
     }
   }
 
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -32,6 +32,17 @@ class UserService {
 
     return { ...tokens, user: userDto };
   }
+
+  async activate(activationLink) {
+    const user = await userModel.findOne({ activationLink });
+
+    if (!user) {
+      throw new Error("No User with provided activation link.");
+    }
+
+    user.isActivated = true;
+    await user.save();
+  }
 }
 
 module.exports = new UserService();
